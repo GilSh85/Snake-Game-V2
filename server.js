516f1047e96b9c16ff4c413d1df34495b8e20a52
GilSh85 <118055345+GilSh85@users.noreply.github.com>
2024-10-30 11:13:48 +0200

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 const dbConfig = {
     host: 'localhost',
     user: 'root',
-    password: 'Gs235331!',
+    password: '***********',
     database: 'snake_game'
 };
 
